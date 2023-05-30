7f004a1ca4214442814410b0bd0cf14fbbd92a95
Piterwork <piterwork3@gmail.com>
2023-05-30 01:02:38 -0300

    Implementação do banco de dados e suas conecções.

---
+++ data_base/conexao.php
@@ -0,0 +1,15 @@
+<?php
+
+    $hostname = "localhost";
+    $bancodedados = "starnordestino";
+    $usuario = "root";
+    $senha = "";
+
+    $conexao = new mysqli ($hostname, $usuario, $senha, $bancodedados);
+    if ($conexao->connect_errno){
+        echo "Falha ao conectar: (" . $mysqli->connect_errno . ")" . $mysqli->connect_errno;
+    }
+    else
+        echo "Conectado ao Banco de Dados";
+
+?>
