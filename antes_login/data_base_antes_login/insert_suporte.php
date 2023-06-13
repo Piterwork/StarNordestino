674953218eddb0a620b04003134adb821dd9cd63
PiterWork <piterwork3@gmail.com>
2023-06-13 13:10:00 -0300

    algumas resftrições

--- antes_login/data_base_antes_login/insert_suporte.php
+++ antes_login/data_base_antes_login/insert_suporte.php
@@ -1,5 +1,15 @@
 <?php
 
+    $nome_suporte = ($_POST['nome_suporte']);
+    $email_suporte = ($_POST['email_suporte']);
+    $mensagem_suporte = ($_POST['mensagem_suporte']);
+
+    if (empty ($nome_suporte) || empty ($email_suporte) || empty ($mensagem_suporte) ) {
+        
+        header('Location: ../respostas/campo_vazio.html');
+
+    } else {
+       
     include ('conexao.php');
 
     $nome_suporte = ($_POST['nome_suporte']);
@@ -9,5 +19,9 @@
     $sql = "INSERT INTO starnordestino.suporte(id_suporte, nome_suporte, email_suporte, mensagem_suporte) values(null, '$nome_suporte', '$email_suporte', '$mensagem_suporte')";
 
     $result = mysqli_query($conexao, $sql);
+
+    header('Location: ../respostas/dados_enviados.html');
+
+    }
     
 ?>
