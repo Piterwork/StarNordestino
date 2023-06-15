866f3ebe0dbc5a8ce50e36a012ec3fc1cc10eb23
PiterWork <piterwork3@gmail.com>
2023-06-15 11:35:05 -0300

    novo banco

--- starnordestino.sql
+++ starnordestino.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 14/06/2023 às 09:17
+-- Tempo de geração: 15/06/2023 às 06:49
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
