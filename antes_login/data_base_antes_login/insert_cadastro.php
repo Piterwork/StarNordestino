87229621236066da57c36ce6ca2b7df8023eccea
PiterWork <piterwork3@gmail.com>
2023-06-15 11:52:24 -0300

    criação de resposta para cadastro

--- antes_login/data_base_antes_login/insert_cadastro.php
+++ antes_login/data_base_antes_login/insert_cadastro.php
@@ -2,14 +2,25 @@
 
     include ('conexao.php');
 
-    $nome_usuario = ($_POST['nome_usuario']);
-    $email = ($_POST['email']);
-    $senha = ($_POST['senha']);
+    $verificador_email = $_POST['email'];
 
-    $sql = "INSERT INTO starnordestino.cadastro(id_cadastro, nome_usuario, email, senha) values(null, '$nome_usuario', '$email', '$senha')";
+    $sql = "SELECT * FROM starnordestino.cadastro WHERE email = '$verificador_email' ";
 
-    $result = mysqli_query($conexao, $sql);
-    
-    header('Location: ../../apos_login/perfil/perfil.php');
+    $result = $conexao -> query($sql);
 
+		if(mysqli_num_rows($result) >= 1) {
+			// header('Location: ../../apos_login/respostas/resposta_agendamento.html');
+			header('Location: ../respostas/resposta_cadastro_email.html');
+        }
+        else {
+            $nome_usuario = ($_POST['nome_usuario']);
+            $email = ($_POST['email']);
+            $senha = ($_POST['senha']);
+        
+            $sql = "INSERT INTO starnordestino.cadastro(id_cadastro, nome_usuario, email, senha) values(null, '$nome_usuario', '$email', '$senha')";
+        
+            $result = mysqli_query($conexao, $sql);
+            
+            header('Location: ../../apos_login/perfil/perfil.php');            
+        }
 ?>
